cf6cee9a40f5a551848e0eecc2665315f90cdee8
a32karats <a32karats@yahoo.co.jp>
2020-11-14 14:29:06 +0900

    commit

---
+++ database/seeders/PpTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PpTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'taro',
+            'age' => 10,
+        ];
+        DB::table('pp')->insert($param);
+
+        $param = [
+            'name' => 'jiro',
+            'age' => 20,
+        ];
+        DB::table('pp')->insert($param);
+
+        $param = [
+            'name' => 'saburo',
+            'age' => 30,
+        ];
+        DB::table('pp')->insert($param);
+    }
+ }
